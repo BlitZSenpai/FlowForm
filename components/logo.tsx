7be6acb75f497f6a5c7774fcf66b1d88e4de3892
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-31 21:37:42 +0530

    add: logo component and new theme colors

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export const Logo = () => {
-  return <div className="">logo</div>;
+  return (
+    <Link
+      href="/"
+      className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer">
+      FlowForm
+    </Link>
+  );
 };
