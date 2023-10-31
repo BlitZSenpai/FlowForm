20216d0e0afdc28605f2085c30bdb2c2ed029b93
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-31 21:28:19 +0530

    add: theme switcher

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,18 @@
+import { Logo } from "@/components/logo";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 import { UserButton } from "@clerk/nextjs";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <Logo />
-      <ThemeSwitcher />
-      <UserButton afterSignOutUrl="/sign-in" />
-      {children}
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
+        <Logo />
+        <div className="flex gap-4 items-center">
+          <ThemeSwitcher />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+      <main className="flex flex-grow w-full">{children}</main>
     </div>
   );
 };
