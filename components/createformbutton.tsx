bd6b50479aa2053cfbfa6db09ace3ec04f8fef0d
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-11-02 21:46:37 +0530

    add: form name field

--- components/createformbutton.tsx
+++ components/createformbutton.tsx
@@ -13,13 +13,26 @@ import {
 import { Button } from "./ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
 
 const formSchema = z.object({
   name: z.string().min(4),
   description: z.string().optional(),
 });
 
+type formSchemaType = z.infer<typeof formSchema>;
+
 export const CreateFormButton = () => {
+  const form = useForm<formSchemaType>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (values: formSchemaType) => {
+    console.log(values);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -30,6 +43,23 @@ export const CreateFormButton = () => {
           <DialogTitle>Create Form</DialogTitle>
           <DialogDescription>Create a new form to start collecting responses</DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
